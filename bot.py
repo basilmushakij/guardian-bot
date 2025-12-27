c23e5bc38e1f66f49b2cee721b5865eec122216c
basilmushakij <bazilmachakij@gmail.com>
2025-12-27 16:29:45 +0700

    Simplify bot status message in home route

--- bot.py
+++ bot.py
@@ -13,11 +13,12 @@ import asyncio
 
 from flask import Flask
 from threading import Thread
+
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alive! The bot is running."
+    return "I'm alive!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
